3d9e1bfb63c4fe0333e7c2a1ee6c638c6c96e03d
Joao Sa <joao.sa@innvent.com.br>
2013-10-24 01:11:52 -0200

    Add nginx optimizations to default configuration.
    - Also disable serving default

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -7,17 +7,39 @@ class nginx {
   } ->
   package { "nginx":
     ensure => latest
-  } ->
+  }
+
   file { "/srv/www":
     require => Package['nginx'],
     ensure => directory,
     owner => "www-data",
     group => "www-data",
-    mode => 755,
+    mode => 0755,
     recurse => true
   } ->
   user { "ubuntu":
     groups => "www-data"
+  }
+
+  service { "nginx":
+    require => Package['nginx'],
+    ensure => running,
+    enable => true,
+    hasrestart => true
+  }
+
+  file { "/etc/nginx/nginx.conf":
+    require => Package['nginx'],
+    notify => Service["nginx"],
+    ensure => present,
+    owner => "root",
+    group => "root",
+    mode => 0644,
+    source => "puppet:///modules/nginx/nginx.conf"
+  } ->
+  file { "/etc/nginx/sites-enabled/default":
+    notify => Service["nginx"],
+    ensure => absent
   }
 
   if $::ec2_ami_id and "xvdg1" in $::lsblk {
