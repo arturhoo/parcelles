6721404246e4b24e50888f86e41861f7de2e881b
Joao Sa <joao.sa@innvent.com.br>
2013-10-28 12:05:05 -0200

    Added common dependecies to Ruby projects
    - imagemagick and sqlite3

--- puppet/modules/ruby/manifests/init.pp
+++ puppet/modules/ruby/manifests/init.pp
@@ -3,6 +3,11 @@ class ruby (
   $version = "1.9.3-p448"
 ) {
   # YUI Compressor gem needs it
+
+  # Common deps for ruby gems from most ruby projects.
+  package { [ "imagemagick", "libmagickwand-dev", "libsqlite3-dev" ]:
+    ensure => latest
+  }
   require java
 
   rbenv::install { $user: }
