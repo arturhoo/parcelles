d86bc99631b10fa5bd1aaa9366119f7510972ac0
Artur Rodrigues <arturhoo@gmail.com>
2014-03-17 18:06:25 -0300

    Replace vagrant user with ubuntu

--- scripts/rbenv.sh
+++ scripts/rbenv.sh
@@ -15,7 +15,7 @@ apt-get install zlib1g-dev curl libssl-dev libsqlite3-dev nodejs \
                 libreadline-gplv2-dev libtinfo-dev imagemagick tklib \
                 libmagickwand-dev oracle-java7-installer libxml2 libxml2-dev \
                 libxslt1-dev -y
-su - vagrant -c 'curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash'
+su - ubuntu -c 'curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash'
 
 read -d '' RBENV_SNIPPET <<"EOF"
 export RBENV_ROOT="${HOME}/.rbenv"
@@ -25,4 +25,4 @@ if [ -d "${RBENV_ROOT}" ]; then
   eval "$(rbenv init -)"
 fi
 EOF
-echo "$RBENV_SNIPPET" | tee -a /home/vagrant/.profile
+echo "$RBENV_SNIPPET" | tee -a /home/ubuntu/.profile
