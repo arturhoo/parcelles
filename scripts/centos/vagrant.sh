38b5990a9e9a31d6ac71e39c16c998bcb2b8e101
Artur Rodrigues <arturhoo@gmail.com>
2014-03-27 15:02:21 -0300

    Use common scripts across jsons

--- scripts/centos/vagrant.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eux
-
-mkdir /home/centos/.ssh
-wget --no-check-certificate \
-    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
-    -O /home/centos/.ssh/authorized_keys
-chown -R centos /home/centos/.ssh
-chmod -R go-rwsx /home/centos/.ssh
--- scripts/centos/vboxga.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-mkdir /tmp/vbox
-VER=$(cat /home/centos/.vbox_version)
-mount -o loop /home/centos/VBoxGuestAdditions_$VER.iso /tmp/vbox
-sh /tmp/vbox/VBoxLinuxAdditions.run
-umount /tmp/vbox
-rmdir /tmp/vbox
-rm /home/centos/*.iso
