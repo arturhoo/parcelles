1fcfcb7cf197b54d2db3c1658fc18b2d62f83dcd
Joao Sa <joao.sa@innvent.com.br>
2014-04-23 17:28:49 -0300

    Move cloud-init install to provisioning stage
    - Fix interference with other scripts

--- scripts/ubuntu/non-aws.sh
+++ scripts/ubuntu/non-aws.sh
@@ -5,3 +5,5 @@ cat /etc/sudoers > /tmp/sudoers.tmp
 echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' >> /tmp/sudoers.tmp
 install -o root -g root -m 0440 /tmp/sudoers.tmp /etc/sudoers
 rm /tmp/sudoers.tmp
+
+apt-get install cloud-init cloud-initramfs-growroot -qy
