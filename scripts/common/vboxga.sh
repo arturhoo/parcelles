a656385774f67f1ad8e2caca1a254c008062f1bc
Joao Sa <joao.sa@innvent.com.br>
2014-04-22 23:12:16 -0300

    Ensure deps to compile vbox modules present
    - make, gcc, and kernel headers

--- scripts/common/vboxga.sh
+++ scripts/common/vboxga.sh
@@ -5,6 +5,7 @@ OS_USER="$SUDO_USER"
 mkdir /tmp/vbox
 VER=$(cat /home/$OS_USER/.vbox_version)
 mount -o loop /home/$OS_USER/VBoxGuestAdditions_$VER.iso /tmp/vbox
+apt-get install make gcc linux-headers-generic -qy
 sh /tmp/vbox/VBoxLinuxAdditions.run
 umount /tmp/vbox
 rmdir /tmp/vbox
