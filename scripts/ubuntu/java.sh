38a4a34686f29b5f27bc03ed2b4b31a9d0f7d856
Joao Sa <joao.sa@innvent.com.br>
2014-05-21 06:19:40 -0300

    Separate java installation from rbenv script
    - aws monitoring agents require a JRE so it show be standard even on PHP
      installs

---
+++ scripts/ubuntu/java.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eux
+
+apt-add-repository -y ppa:webupd8team/java
+apt-get update -q
+echo debconf shared/accepted-oracle-license-v1-1 select true | \
+     debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+     debconf-set-selections
+
+apt-get install oracle-java7-installer -qy
--- scripts/ubuntu/rbenv.sh
+++ scripts/ubuntu/rbenv.sh
@@ -1,14 +1,8 @@
 #!/bin/bash -eux
 
-apt-add-repository -y ppa:webupd8team/java
-apt-get update -q
-echo debconf shared/accepted-oracle-license-v1-1 select true | \
-     debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-     debconf-set-selections
 apt-get install zlib1g-dev curl libssl-dev libsqlite3-dev nodejs \
                 libreadline-gplv2-dev libtinfo-dev imagemagick tklib \
-                libmagickwand-dev oracle-java7-installer libxml2 libxml2-dev \
+                libmagickwand-dev libxml2 libxml2-dev \
                 libxslt1-dev -q -y
 su - ubuntu -c 'curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash'
 
