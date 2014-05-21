638e35ee43ca2c52501ebfc0e80a3b6486f48fd1
Joao Sa <joao.sa@innvent.com.br>
2014-05-21 17:01:00 -0300

    Fix mongodb provisioning script
    - Add packer config to generate a base image with Ruby/MongoDB

--- scripts/ubuntu/mongodb-aws.sh
+++ scripts/ubuntu/mongodb-aws.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -eux
 
-apt-get update -q
-apt-get install xfsprogs -q -y
+apt-get install xfsprogs -qy
 
 if [ -b /dev/xvdf ]; then
     (echo n; echo p; echo 1; echo ; echo ; echo w;) | fdisk /dev/xvdf
     mkfs.xfs /dev/xvdf1
     echo "/dev/xvdf1      /var/lib/mongodb xfs noatime,noexec,nodiratime 0 0" >> /etc/fstab
-    mount -a
+    mkdir -m 000 /var/lib/mongodb
+    mount /var/lib/mongodb
 fi
